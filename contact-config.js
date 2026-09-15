4e0657f4c126747d3cdef748a849af438a2074a4
Strikerliker <57738905+Strikerliker@users.noreply.github.com>
2026-09-15 00:06:32 -0400

    Connect portfolio contact form to Lambda Function URL

--- contact-config.js
+++ contact-config.js
@@ -1,3 +1,3 @@
 // Runtime contact endpoint configuration.
 // Keep email addresses and credentials out of browser code.
-window.CONTACT_API_URL = window.CONTACT_API_URL || '';
+window.CONTACT_API_URL = 'https://v5u4ucab2mc3lznhms54fkgwfe0tfejo.lambda-url.us-east-2.on.aws/';
